b6a17d2a778a5c2bcfe1d93cb54c94b38844243e
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2019-12-21 15:44:31 +0400

    chore(VersionControll): squash CRUD commits
    feat(Accounts) create Accounts context, add User schema
    
    chore(Accounts): remove unused import
    
    chore(Accounts): remove unused context apis
    
    chore(Accounts): import Repo functions instead of aliasing
    
    feat(Accounts): add /users CRUD API + UserView
    
    feat(Accounts): create Accounts context, add User schema (#1)
    
    * feat(Accounts) create Accounts context, add User schema
    
    * chore(Accounts): remove unused import
    
    * chore(Accounts): remove unused context apis
    
    * chore(Accounts): import Repo functions instead of aliasing

---
+++ lib/hopper_web/controllers/user_controller.ex
@@ -0,0 +1,69 @@
+defmodule HopperWeb.UserController do
+  use HopperWeb, :controller
+
+  alias Hopper.Accounts
+
+  def index(conn, _params) do
+    users = Accounts.list_users()
+
+    conn
+    |> put_status(:ok)
+    |> render("index.json", users: users)
+  end
+
+  def show(conn, %{"id" => id}) do
+    case Accounts.get_user(id) do
+      {:ok, user} ->
+        conn
+        |> put_status(:ok)
+        |> render("show.json", user: user)
+
+      {:error, 404} ->
+        conn
+        |> put_status(:not_found)
+        |> text("not found")
+    end
+  end
+
+  def create(conn, %{"user" => user}) do
+    case Accounts.create_user(user) do
+      {:ok, user} ->
+        conn
+        |> put_status(:ok)
+        |> render("show.json", user: user)
+
+      {:error, _} ->
+        conn
+        |> put_status(:bad_request)
+        |> text("Bad Request")
+    end
+  end
+
+  def update(conn, %{"user" => user, "id" => id}) do
+    case Accounts.update_user(id, user) do
+      {:ok, user} ->
+        conn
+        |> put_status(:ok)
+        |> render("show.json", user: user)
+
+      {:error, _} ->
+        conn
+        |> put_status(:bad_request)
+        |> text("Bad Request")
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    case Accounts.delete_user(id) do
+      :ok ->
+        conn
+        |> put_status(:ok)
+        |> text("User Deleted")
+
+      {:error, 404} ->
+        conn
+        |> put_status(:not_found)
+        |> text("Not Found")
+    end
+  end
+end
